c390a8f6024c2ee9f4e3b3e9fcc9d0cc3381be25
Takuya Shizukuishi <b1015208@fun.ac.jp>
2018-12-09 13:10:16 +0900

    [add] material ui

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import "ui/styles/index.scss";
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './ui/components/App';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from "ui/styles/theme";
+
+import App from 'ui/components/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import store from './store/store';
+import store from 'store/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
   </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/ui/components/App.js
+++ src/ui/components/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import * as types from "infrastructure/types";
-import bindActionCreator from "ui/bindActionCreator";
+import Counter from "./Counter";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <button onClick={() => bindActionCreator(types.COUNT_UP)}>
-          {this.props.count}
-        </button>
-      </div>
+      <Counter />
     )
   }
 }
 
-export default connect(state => state)(App);
+export default App;
 
